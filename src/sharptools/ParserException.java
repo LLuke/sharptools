d4447ec5e5c1a65151eb1a93bbf1b28b2c734d53
oleglebedev <oleglebedev>
2001-11-15 23:21:00 +0000

    no message

---
+++ src/sharptools/ParserException.java
@@ -0,0 +1,61 @@
+/*
+ * @(#)ParserException.java
+ * 
+ * $Id: ParserException.java,v 1.1.1.1 2001/11/03 05:39:14 huaz Exp $
+ * 
+ * Created on October 28, 2000, 6:26 PM
+ */
+
+/**
+ * This Exception is raised when Formula fails in tokenizing or parsing the
+ * formula.
+ *
+ * @author Hua Zhong <huaz@cs.columbia.edu>
+ * @version $Revision: 1.1.1.1 $
+ */
+public class ParserException extends Exception {
+    private boolean quiet;
+    private String msg;
+
+    /**
+     * Contructor for ParserException.  By default, sets quiet to true.
+     */
+    public ParserException() { quiet = true; };
+
+    /**
+     * @param msg the error message string 
+     */
+    public ParserException(String msg) { super(msg); this.msg = msg; };
+
+    /**
+     * @param msg the error object
+     */
+    public ParserException(Object msg) {
+	super(msg.toString());
+	this.msg = msg.toString();
+    };
+
+    /**
+     * This returns the value of quiet.
+     *
+     * @return true if quiet is true, false otherwise
+     */
+    public boolean isQuiet() { return quiet; }
+
+    /**
+     * toString method for ParserException.
+     *
+     * @return the error message string
+     */
+    public String toString() { return msg; }
+}
+
+
+
+
+
+
+
+
+
+
